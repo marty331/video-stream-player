f75b3ae867e6b8d815f6a8f6308ed516b4625887
Marty Ballard <marty331@gmail.com>
2021-08-27 20:10:57 -0500

    cleanup

--- server/cleaner.js
+++
@@ -1,6 +0,0 @@
-const findRemoveSync = require('find-remove')
-
-setInterval(() => {
-    var result = findRemoveSync('/Users/marty331/Movies/videos/second', { age: { seconds: 30 }, extensions: '.ts' });
-    console.log(result);
-}, 20000);
